f9ce63d8e9d2cd76d7fe3b64c16db27a2fc90018
FredCamaragit <fcamara14@gmail.com>
2017-05-04 21:17:21 -0500

    Added HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", function(req, res){
+router.get("/", function(req, res) {
   res.render("index");
 });
 
+router.get("/activities", function(req, res){
+	res.render("partials/activities");
+});
+
+router.get("/account", function(req, res){
+	res.render("partials/login");
+});
 
 module.exports = router;
