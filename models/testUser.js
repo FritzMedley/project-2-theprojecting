c7c18ffdec9ac5c6fb0e655af03f03c495625b0e
MagentaReds <grantodaniels@gmail.com>
2017-05-05 21:41:51 -0500

    Test model for users

---
+++ models/testUser.js
@@ -0,0 +1,27 @@
+
+module.exports = function(sequelize, DataTypes){
+  var User = sequelize.define("User", {
+    oauthId: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    credType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+
+  return User;
+};
