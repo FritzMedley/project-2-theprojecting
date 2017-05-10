04cfdaf7262aff5027638675855d76777cfa8e47
MagentaReds <grantodaniels@gmail.com>
2017-05-10 15:40:09 -0500

    Added more seed values

--- config/dummySeeds.js
+++ config/dummySeeds.js
@@ -8,14 +8,22 @@ module.exports = function() {
     password: "123"
   }).done(function(){});
 
+  db.User.create({
+    name: "No Way",
+    email: "no@no.com",
+    credType: "local",
+    password: "123"
+  }).done(function(){});
+
   db.Event.create({
     description: "Test",
     name: "Ttest Name",
-    numAttendees: "1",
+    numAttendees: "2",
     location: "Austin, Tx",
     creatorId: 1
   }).done(function(dbEvent){
     dbEvent.addUser(1);
+    dbEvent.addUser(2);
   });
 
   db.Event.create({
@@ -26,6 +34,16 @@ module.exports = function() {
     creatorId: 1
   }).done(function(dbEvent){
     dbEvent.addUser(1);
+  });
+
+  db.Event.create({
+    description: "My lover",
+    name: "Billie Jean is not",
+    numAttendees: "3",
+    location: "Taylor, Tx",
+    creatorId: 2
+  }).done(function(dbEvent){
+    dbEvent.addUser(2);
   });
 
 };
