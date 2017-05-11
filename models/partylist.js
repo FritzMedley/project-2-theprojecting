e689958d6b59ab0b09b805c5fa204ad9bf36c8b3
MagentaReds <grantodaniels@gmail.com>
2017-05-11 05:32:04 -0500

    Removed old unworking code

--- models/partylist.js
+++ models/partylist.js
@@ -1,41 +1,4 @@
 module.exports = function(sequelize, DataTypes)  {
-  // var Partylist = sequelize.define("Partylist", {
-  //   // UserId: {
-  //   //   type: DataTypes.INTEGER,
-  //   //   allowNull: false,
-  //   //   references: {
-  //   //     model: "User",
-  //   //     key: "id"
-  //   //   }
-  //   // },
-  //   // EventId: {
-  //   //   type: DataTypes.INTEGER,
-  //   //   allowNull: false,
-  //   //   references: {
-  //   //     model: "Event",
-  //   //     key: "id"
-  //   //   }
-  //   // },
-  // },
-  // {
-  //   classMethods: {
-  //     associate: function(models) {
-  //       // Associating Author with Posts
-  //       // When an Author is deleted, also delete any associated Posts
-  //       Partylist.hasMany(models.Event,{
-  //         foreignKey: {
-  //           name: "EventId",
-  //           allowNull: false
-  //         }
-  //       });
-  //       Partylist.hasMany(models.User,{
-  //         foreignKey: {
-  //           name: "UserId",
-  //           allowNull: false
-  //         }
-  //       });
-  //   }
-  // }});
 
   var Partylist = sequelize.define("Partylist", {},{
     // We're saying that we want our Author to have Posts
