fb1fef84098b42cf72b14adb6069a398cb0d233f
Josh Yap <joshua.c.yap@gmail.com>
2017-05-11 17:22:52 -0500

    committing some small mocha test updates

--- tests/test.js
+++ tests/test.js
@@ -16,3 +16,43 @@ describe('/', function() {
       })
   });
 });
+describe('/findevent', function() {
+  it('returns all events', function(done) {
+    chai.request(server)
+      .get('/findevent')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('create a new event', function(done) {
+    chai.request(server)
+      .post('/createevent')
+      .set('content-type', 'application/json')
+      .send({name: 'Mace Windu', type: 'Master'})
+      .end(function(err, res) {
+        res.should.have.status(204);
+        done();
+      });
+  });
+});
+describe('/login', function() {
+  it('return a login page', function(done) {
+    chai.request(server)
+      .get('/siths')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('creates a new account', function(done) {
+    chai.request(server)
+      .post('/siths')
+      .set('content-type', 'application/json')
+      .send({name: 'Darth Maul', type: 'Lord'})
+      .end(function(err, res) {
+        res.should.have.status(204);
+        done();
+      });
+  });
+});
