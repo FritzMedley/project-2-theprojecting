cbdd0acc3861101dcfab94940f96a50d2a751100
FredCamaragit <fcamara14@gmail.com>
2017-05-10 19:05:42 -0500

    created eventRoutes, updated events.js and created a singleevent page, updated existed createevent page

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -9,7 +9,7 @@ router.get("/activities", function(req, res){
   // var resObject = {
   //   loggedIn: req.isAuthenticated()
   // }
-  db.Event.findAll({limit: 10}).then(function(dbEvents){
+  db.Event.findAll({limit: 250}).then(function(dbEvents){
   	var hbsObject = {event: dbEvents};
   	res.render("./partials/activities", hbsObject);
   });
@@ -34,55 +34,66 @@ router.post("/createevent", function(req, res){
     var newEvent = {
       description: req.body.description,
       name: req.body.name,
-      numAttendees: req.body.numAttendees,
+      numAttendees: parseInt(req.body.numAttendees),
       category: req.body.category,
       location: req.body.location,
       startTime: req.body.startTime, 
       endTime: req.body.endTime,
       date: req.body.month + " " + req.body.day + " , " + req.body.year,
       creatorId: req.user.id,
+      image: req.body.image
     };
+    if(!req.body.image) {
+    	newEvent.image = "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg"
+    }
+    //store the event in the dabtabase
     db.Event.create(newEvent).then(function(dbEvent){
-     // dbEvent.addUser(req.user.id);
-      //after the event created, redirect the
-      //user to an individual event page labeled by ID in database
-      res.redirect("/event?id="+ dbEvent.id);   
+    	console.log(dbEvent);
+    	console.log(dbEvent.id);
+     dbEvent.addUser(req.user.id);
+     //  after the event created, redirect the
+     //  user to an individual event page labeled by ID in database 
+    res.json({redirect: "/event/?id="+dbEvent.id});   
     });
   }
 });
 
+
 router.get("/event", function(req, res){
-	var hbsObject = {event: res.body}
+  if(req.query.id !== null) {
+  	console.log(req.query.id);
+     db.Event.findOne({
+        where: {
+           id: req.query.id
+        }
+    }).done(function(dbEvent){
+     if(dbEvent === null) {
+     	//console.log("DB-Event is equalling null");
+      res.redirect("/activities")
+     } else {
+     	var hbsObject = {event: dbEvent}
+     	console.log(hbsObject);
+      res.render("./partials/singleevent", hbsObject)
+     }
+    }, function(err){
+        if (err) throw err;
+        res.redirect("/activities");
+    });
+  }
+  else {
+  	//console.log("This is the one that's happening.")
+    res.redirect("/activities");
+  }
+});
 
-	res.render("./partials/singleevent", hbsObject)
+// router.get("/findactivities", function(req, res){
+// 	//need to pass in query
+// 	db.Event.findAll({
+// 		where {
+// 			firstName: req.query.firstName;
+// 		}
 
-})
-
-// router.get("/event?", function(req, res){
-//   if(req.query.id !== null) {
-//   	console.log(req.query.id);
-//      db.Event.findOne({
-//         where: {
-//            id: req.query.id
-//         }
-//     }).done(function(dbEvent){
-//      if(dbEvent === null) {
-//      	//console.log("DB-Event is equalling null");
-//       res.redirect("/activities")
-//      } else {
-//      	var hbsObject = {event: dbEvent}
-//      	console.log(hbsObject);
-//       res.render("./partials/singleevent", hbsObject)
-//      }
-//     }, function(err){
-//         if (err) throw err;
-//         res.redirect("/activities");
-//     });
-//   }
-//   else {
-//   	//console.log("This is the one that's happening.")
-//     res.redirect("/activities");
-//   }
-// });
+// 	})
+// })
 
 module.exports = router;
